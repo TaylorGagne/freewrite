a71cee8fee9f850fb5e80de2dedee864c6be497a
TaylorGagne <tgagne74@gmail.com>
2015-10-28 12:35:37 -0400

    update

--- db/migrate/20151028043256_create_articles.rb
+++ db/migrate/20151028043256_create_articles.rb
@@ -1,8 +1,57 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.text :content
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
